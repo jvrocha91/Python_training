15156764072dac163878b88b8b5a2cc4455e49e1
jvrocha91 <107816755+jvrocha91@users.noreply.github.com>
2023-03-06 21:16:33 -0300

    Realiza a soma de dois valores

---
+++ somanumeros.py
@@ -0,0 +1,19 @@
+
+nome=input("Digite seu nome:")
+print("-----------")
+           
+nb1=int(input("Digite um numero:"))
+
+nb2=int(input("Digite outro numero:"))
+print("-----------")
+
+soma=(nb1+nb2)
+
+
+print(f"{nome} escolheu os numeros {nb1} e {nb2} e sua soma e {soma}")
+print("-----------")           
+           
+
+           
+
+           

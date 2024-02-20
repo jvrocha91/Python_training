52e35efeb52072b6794c578aa76704ba41858fa0
João Victor Bezerra Batista Rocha <107816755+jvrocha91@users.noreply.github.com>
2024-02-19 23:39:10 -0300

    Hangman

---
+++ Hangman/hang_words.py
@@ -0,0 +1,12 @@
+lista_animais = [
+    "leao", "tigre", "elefante", "girafa", "zebra",
+    "hipopotamo", "rinoceronte", "panda", "urso", "lobo",
+    "raposa", "cervo", "coelho", "esquilo", "tartaruga",
+    "crocodilo", "cobra", "lagarto", "sapo", "perereca",
+    "aguia", "falcao", "papagaio", "tucano", "coruja",
+    "pinguim", "golfinho", "baleia", "tubarao", "polvo",
+    "camarao", "caranguejo", "piranha", "cavalo", "ouriço",
+    "macaco", "chimpanze", "gorila", "orangotango", "rato",
+    "morcego", "quati", "tamandua", "preguiça", "capivara",
+    "jaguar", "onça", "anta", "arara", "flamingo"
+]
---
+++ Hangman/hangman.py
@@ -0,0 +1,43 @@
+import random 
+import hang_words
+from hang_art import logo, stages
+
+
+print(logo)
+hang_word = random.choice(hang_words.lista_animais)
+lives=6
+display = ["_" for _ in hang_word]
+print(display)
+
+
+end_game=False
+while not end_game:
+    guess = input("Escolha uma letra: ").lower()
+    if guess in display:
+        print(f"\nVocê já escolheu a letra {guess}")
+    else:
+        for position in range(len(hang_word)):
+            letter = hang_word[position]
+            if letter == guess:
+                display[position] = letter
+
+        if guess not in hang_word:
+            lives-=1
+            print(f"\nVocê escolheu uma letra {guess}, que não está na palavra. Você perdeu uma vida.")
+    
+    print(stages[6 - lives])
+    print(display)
+
+    if "_" not in display:
+        end_game = True
+        print("Parabens! Você ganhou")
+    elif lives == 0:
+        end_game = True
+        print(f"\nVocê perdeu! A palavra era '{hang_word}'.")
+
+
+
+
+
+
+

f3193bbbfc78afc0e32133fe542bd3e744cc9244
João Victor Bezerra Batista Rocha <107816755+jvrocha91@users.noreply.github.com>
2024-03-13 20:48:21 -0300

    Update auction.py

--- auction.py
+++ auction.py
@@ -1,22 +1,34 @@
+import os
 def menu_mensage():
     print("============")
     print("Bem vindo ao leilão do João!\n")
 
+def bid_winner(players):
+    maior_valor = 0
+    winner=""
+    for nome in players:
+        valor_atual = players[nome]
+        if valor_atual > maior_valor:
+            maior_valor = valor_atual
+            winner = nome
+    print(f"O ganhador do leilão foi {winner} com a aposta de {maior_valor} Reais!")
+          
 def main():
     menu_mensage()
     players={}
     while True:
         nome=input("Digite o nome do comprador:")
-        bid=int(input("Qual valor vai apostar:"))
+        bid=int(input("Qual valor vai apostar: R$"))
         players[nome]=bid
-        final_bid=int(input("Existe algum outro apostador ?:\nDigite '1' ou '2':"))
-        if final_bid == 1:
+        final_bid=(input("Existe algum outro apostador ?:Digite 'sim ou 'nao':"))
+        if final_bid == "sim":
+            os.system('cls' if os.name == 'nt' else 'clear')
             continue
-        if final_bid == 2:
+        if final_bid == "nao":
+            os.system('cls' if os.name == 'nt' else 'clear')
             break
+    bid_winner(players)
     
-    print( {players.keys()}+"---"+{players.values()})
-
 main()
 
 

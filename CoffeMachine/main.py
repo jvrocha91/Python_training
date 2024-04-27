f0bbbe144f85a22be4f9813bd993dbd06e77c872
João Victor Bezerra Batista Rocha <107816755+jvrocha91@users.noreply.github.com>
2024-04-27 02:35:26 -0300

    Update main.py

--- CoffeMachine/main.py
+++ CoffeMachine/main.py
@@ -4,7 +4,7 @@ MENU = {
             "water": 50,
             "coffee": 18,
         },
-        "cost": 1.5,
+        "cost": 7.5,
     },
     "latte": {
         "ingredients": {
@@ -12,7 +12,7 @@ MENU = {
             "milk": 150,
             "coffee": 24,
         },
-        "cost": 2.5,
+        "cost": 12.5,
     },
     "cappuccino": {
         "ingredients": {
@@ -20,7 +20,7 @@ MENU = {
             "milk": 100,
             "coffee": 24,
         },
-        "cost": 3.0,
+        "cost": 15.0,
     }
 }
 resources = {
@@ -29,62 +29,60 @@ resources = {
     "coffee": 100,
 }
 
-POWER = True
 MONEY = 0
 
 def print_resources():
     print(f"Agua:{resources['water']}ml\nLeite:{resources['milk']}ml\nCafé:{resources['coffee']}g\nDinheiro:R${MONEY}")
 
 def check_resources(choice):
-    print(f"Making your {choice}. Please wait...")
-    for i , necessario in MENU[choice]['ingredients'].items():
+    print(f"\nPreparando seu {choice}. Por favor esperar...")
+    for i, necessario in MENU[choice]['ingredients'].items():
         if resources[i] < necessario:
-            print("Desculpa, não temos {i} o suficiente")
+            print(f"Desculpa, não temos {i} suficiente.")
             return False
     return True
 
+def update_resources(bill, choice):
+    global MONEY
+    MONEY += bill
+    for i, utilizado in MENU[choice]['ingredients'].items():
+        resources[i] -= utilizado
+
 def pagamento(choice):
     print("Por favor, realizar pagamento.\n")
-    bill=MENU[choice]['cost']
-    centavos=int(input("Quantas moedas de 50 centavos?:"))
+    bill = MENU[choice]['cost']
+    centavos = int(input("Quantas moedas de 50 centavos?:"))
     um_real = int(input("Quantas moedas de 1 Real?:"))
     dois_reais = int(input("Quantas notas de 2 Reais?:"))
     cinco_reais = int(input("Quantas notas de 5 Reais?:"))
-    valor_input = (centavos*0.5)+(um_real)+(dois_reais*2)+(cinco_reais*5)
-    return check_pagamento(bill,valor_input,choice)
+    valor_input = (centavos * 0.5) + (um_real) + (dois_reais * 2) + (cinco_reais * 5)
+    return check_pagamento(bill, valor_input, choice)
 
-def check_pagamento(bill,valor_input,choice):
+def check_pagamento(bill, valor_input, choice):
+    troco = valor_input - bill
     if valor_input >= bill:
-        troco=valor_input-bill
         print(f"Aqui o seu troco de {troco}R$")
-        update_resources(bill,choice)
+        update_resources(bill, choice)
         return True
     else:
-        print("Valor inserido insuficente, dinheiro restornado")
+        print(f"Valor inserido insuficente,faltam {troco} R$ dinheiro restornado")
         return False
 
-def update_resources(bill,choice):
-    global MONEY
-    MONEY += bill
-    global resources
-    for i in MENU[choice]['ingredients'].items():
-        resources[i]-=
-
-def start_menu():
-    choice = input("O que você deseja ?(espresso/latte/cappuccino)")
-    if choice == "off":
-        global POWER
-        POWER = False
-    elif choice == "report":
-        print_resources()
-    elif choice in MENU:
-        if check_resources(choice):
-            pagamento(choice)
-        else:
-            print("Transação não concluida")
-
 def main():
-    while POWER:
-        start_menu()
+    while True:
+        choice = input("\nO que você deseja ?(espresso/latte/cappuccino):")
+        if choice == "off":
+            break
+        elif choice == "report":
+            print_resources()
+        elif choice in MENU:
+            if check_resources(choice):
+                pago = pagamento(choice)
+                if pago:
+                    print(f"Aqui está seu {choice}.Obrigado e volte sempre!")
+            else:
+                print("Transação não concluida")
+        else:
+            print("Opção invalida,digite novamente")
 
 main()

177b968a569f1cc81c22b6a367d1266d2c69b351
João Victor Bezerra Batista Rocha <107816755+jvrocha91@users.noreply.github.com>
2024-05-18 22:56:39 -0300

    Update main.py

--- art/main.py
+++ art/main.py
@@ -1,12 +1,21 @@
 from turtle import Turtle, Screen
+from random import random
+# Para simplificar poderia ser import turtle as t e depois ao declarar objeto, artist = t.Turtle()
 
 jorge_the_turtle = Turtle()
 jorge_the_turtle.shape("turtle")
 jorge_the_turtle.color("green")
+jorge_the_turtle.speed(1)
+#will_the_turtle = Turtle()
+#will_the_turtle.shape("turtle")
+#will_the_turtle.color("red")
+#will_the_turtle.speed(1)
 
-for i in range(4):
-    jorge_the_turtle.forward(100)
-    jorge_the_turtle.right(90)
+for i in range(15):
+    jorge_the_turtle.forward(10)
+    jorge_the_turtle.penup()
+    jorge_the_turtle.forward(10)
+    jorge_the_turtle.pendown()
 
 screen = Screen()
 screen.exitonclick()

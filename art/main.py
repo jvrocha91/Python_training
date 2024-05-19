94d6081c43b70223de3b0ed0544666ea02e5c0d2
João Victor Bezerra Batista Rocha <107816755+jvrocha91@users.noreply.github.com>
2024-05-19 19:10:30 -0300

    Update main.py

--- art/main.py
+++ art/main.py
@@ -1,21 +1,23 @@
 from turtle import Turtle, Screen
-from random import random
+from random import choice
 # Para simplificar poderia ser import turtle as t e depois ao declarar objeto, artist = t.Turtle()
 
 jorge_the_turtle = Turtle()
 jorge_the_turtle.shape("turtle")
 jorge_the_turtle.color("green")
-jorge_the_turtle.speed(1)
-#will_the_turtle = Turtle()
-#will_the_turtle.shape("turtle")
-#will_the_turtle.color("red")
-#will_the_turtle.speed(1)
+jorge_the_turtle.speed(3)
+jorge_the_turtle.pensize(8)
 
-for i in range(15):
-    jorge_the_turtle.forward(10)
-    jorge_the_turtle.penup()
-    jorge_the_turtle.forward(10)
-    jorge_the_turtle.pendown()
+cores = ["red","blue","green","yellow","purple","orange"]
+numero_lados = 3
+
+while numero_lados <=15:
+    jorge_the_turtle.color(choice(cores))
+    for i in range(numero_lados):
+        angulo = 360 / numero_lados
+        jorge_the_turtle.forward(100)
+        jorge_the_turtle.right(angulo)
+    numero_lados += 1
 
 screen = Screen()
 screen.exitonclick()
